a890096bc18966553f9bafd83c929df5603cbee0
tspoolst <tspoolst@gmail.com>
2022-11-07 14:15:33 -0500

    added git-bash and cygwin subKernelName list

--- .bash_profile.d/main.sh
+++ .bash_profile.d/main.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-for i in main main/$(uname -s) user ; do
-  i=${HOME}/.bash_profile.d/${i}
-  if [[ -d ${i} ]] ; then
-    for j in ${i}/*.sh ; do
-      [[ -s ${j} ]] && . ${j}
+unset ${!lc_dotdbashprofile_*}
+
+lc_dotdbashprofile_kernelName=$(uname -s | tr A-Z a-z)
+lc_dotdbashprofile_kernelName="${lc_dotdbashprofile_kernelName%%_*}"
+if [[ "${lc_dotdbashprofile_kernelName}" =~ (cygwin|mingw64) ]] ; then
+  lc_dotdbashprofile_subKernelPath="main/${lc_dotdbashprofile_kernelName}"
+  lc_dotdbashprofile_kernelPath="main/linux"
+else
+  lc_dotdbashprofile_kernelPath="main/${lc_dotdbashprofile_kernelName}"
+fi
+
+for lc_dotdbashprofile_i in main ${lc_dotdbashprofile_kernelPath} ${lc_dotdbashprofile_subKernelPath} user ; do
+  lc_dotdbashprofile_i=${HOME}/.bash_profile.d/${lc_dotdbashprofile_i}
+  if [[ -d ${lc_dotdbashprofile_i} ]] ; then
+    for lc_dotdbashprofile_j in ${lc_dotdbashprofile_i}/*.sh ; do
+      [[ -s ${lc_dotdbashprofile_j} ]] && . ${lc_dotdbashprofile_j}
     done
   fi
 done
+
+unset ${!lc_dotdbashprofile_*}
